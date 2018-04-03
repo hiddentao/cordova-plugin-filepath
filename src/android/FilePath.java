46caf4cb23c93af4556eaea83499ae2d9ecab744
Christopher Cox <ChrizC@chrizc.co.uk>
2018-04-03 10:27:04 +0100

    Use new-style Android permissions (request at first use) (#25)
    * Use new-style Android permissions (request at first use)
    
    * Fix build errors

--- src/android/FilePath.java
+++ src/android/FilePath.java
@@ -19,6 +19,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PermissionHelper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -41,14 +42,20 @@ public class FilePath extends CordovaPlugin {
     private static final String GET_CLOUD_PATH_ERROR_ID = "cloud";
 
     private static final int RC_READ_EXTERNAL_STORAGE = 5;
+    
+    private static CallbackContext callback;
+    private static String uriStr;
+    
+    public static final int READ_REQ_CODE = 0;
+    
+    public static final String READ = Manifest.permission.READ_EXTERNAL_STORAGE;
+    
+    protected void getReadPermission(int requestCode) {
+        PermissionHelper.requestPermission(this, requestCode, READ);
+    }
 
     public void initialize(CordovaInterface cordova, final CordovaWebView webView) {
         super.initialize(cordova, webView);
-
-        // Check whether we have the read storage permission.
-        if (ActivityCompat.checkSelfPermission(this.cordova.getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this.cordova.getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, RC_READ_EXTERNAL_STORAGE);
-        }
     }
 
     /**
@@ -61,40 +68,22 @@ public class FilePath extends CordovaPlugin {
      */
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        JSONObject resultObj = new JSONObject();
+        this.callback = callbackContext;
+        this.uriStr = args.getString(0);
 
         if (action.equals("resolveNativePath")) {
-             /* content:///... */
-            String uriStr = args.getString(0);
-            Uri pvUrl = Uri.parse(uriStr);
-
-            Log.d(TAG, "URI: " + uriStr);
-
-            Context appContext = this.cordova.getActivity().getApplicationContext();
-            String filePath = getPath(appContext, pvUrl);
-
-            //check result; send error/success callback
-            if (filePath == GET_PATH_ERROR_ID) {
-                resultObj.put("code", GET_PATH_ERROR_CODE);
-                resultObj.put("message", "Unable to resolve filesystem path.");
-
-                callbackContext.error(resultObj);
-            }
-            else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
-                resultObj.put("code", GET_CLOUD_PATH_ERROR_CODE);
-                resultObj.put("message", "Files from cloud cannot be resolved to filesystem, download is required.");
-
-                callbackContext.error(resultObj);
+            if (PermissionHelper.hasPermission(this, READ)) {
+                resolveNativePath();
             }
             else {
-                Log.d(TAG, "Filepath: " + filePath);
-
-                callbackContext.success("file://" + filePath);
+                getReadPermission(READ_REQ_CODE);
             }
 
             return true;
         }
         else {
+            JSONObject resultObj = new JSONObject();
+            
             resultObj.put("code", INVALID_ACTION_ERROR_CODE);
             resultObj.put("message", "Invalid action.");
 
@@ -102,6 +91,53 @@ public class FilePath extends CordovaPlugin {
         }
 
         return false;
+    }
+    
+    public void resolveNativePath() throws JSONException {
+        JSONObject resultObj = new JSONObject();
+        /* content:///... */
+        Uri pvUrl = Uri.parse(this.uriStr);
+
+        Log.d(TAG, "URI: " + this.uriStr);
+
+        Context appContext = this.cordova.getActivity().getApplicationContext();
+        String filePath = getPath(appContext, pvUrl);
+
+        //check result; send error/success callback
+        if (filePath == GET_PATH_ERROR_ID) {
+            resultObj.put("code", GET_PATH_ERROR_CODE);
+            resultObj.put("message", "Unable to resolve filesystem path.");
+
+            this.callback.error(resultObj);
+        }
+        else if (filePath.equals(GET_CLOUD_PATH_ERROR_ID)) {
+            resultObj.put("code", GET_CLOUD_PATH_ERROR_CODE);
+            resultObj.put("message", "Files from cloud cannot be resolved to filesystem, download is required.");
+
+            this.callback.error(resultObj);
+        }
+        else {
+            Log.d(TAG, "Filepath: " + filePath);
+
+            this.callback.success("file://" + filePath);
+        }
+    }
+    
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        for (int r:grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                JSONObject resultObj = new JSONObject();
+                resultObj.put("code", 3);
+                resultObj.put("message", "Filesystem permission was denied.");
+                
+                this.callback.error(resultObj);
+                return;
+            }
+        }
+        
+        if (requestCode == READ_REQ_CODE) {
+            resolveNativePath();
+        }
     }
 
 
